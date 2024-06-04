e53e555e230a4ed7e801a938c7a435a5c8f98869
wei <mai@mai0313.com>
2024-06-05 01:32:33 +0800

    chore: Update pyproject.toml and requirements files

--- scripts/screenshot.py
+++ scripts/screenshot.py
@@ -1,15 +1,25 @@
 from adbutils import adb
+from pydantic import BaseModel
+import autorootcwd
+from src.types.simulator import SimulatorSettings
+from src.utils.config_utils import load_config
 
 
-def screenshots() -> None:
-    adb.connect("127.0.0.1:16416")
-    d = adb.device()
-    running_app = d.app_current()
-    if running_app.package != "com.longe.allstarhmt":
-        d.app_start("com.longe.allstarhmt")
-    current_screent = d.screenshot()
-    current_screent.save("./data/allstars_test/get_rewards.png")
+class Scripts(BaseModel):
+    settings: SimulatorSettings
+
+    def screenshots(self) -> None:
+        serial = f"127.0.0.1:{self.settings.adb_port}"
+        adb.connect(serial)
+        d = adb.device(serial=serial)
+        running_app = d.app_current()
+        if running_app.package != "com.longe.allstarhmt":
+            d.app_start("com.longe.allstarhmt")
+        current_screent = d.screenshot()
+        current_screent.save("./data/allstars_test/debug.png")
 
 
 if __name__ == "__main__":
-    screenshots()
+    settings = load_config("./configs/simulator.yaml")
+    scripts = Scripts(settings=settings)
+    scripts.screenshots()
