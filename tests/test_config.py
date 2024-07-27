d68c82a72366f993b7cae0710184e7920faea3d6
wei <mai@mai0313.com>
2024-07-28 06:41:57 +0800

    chore: Update test_load_hydra_config() to return a dictionary

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,6 +1,6 @@
 from src.utils.config_utils import load_hydra_config
 
 
-def test_load_hydra_config() -> None:
+def test_load_hydra_config() -> dict:
     config = load_hydra_config()
     assert isinstance(config, dict)
