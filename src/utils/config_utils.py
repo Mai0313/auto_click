85f2c76b918f05ba56ffafcb6f4aa51b6548aada
wei <mai@mai0313.com>
2024-07-28 05:52:52 +0800

    chore: Update default game configuration to fix typo in all_stars_cn

--- src/utils/config_utils.py
+++ src/utils/config_utils.py
@@ -1,7 +1,16 @@
-import yaml
+import sys
+
+from hydra import compose, initialize
+from omegaconf import OmegaConf
 
 
-def load_config(path: str) -> dict:
-    with open(path) as f:
-        config = yaml.load(f, Loader=yaml.FullLoader)
-    return config
+def load_hydra_config() -> dict:
+    args = sys.argv[1:]
+    with initialize(config_path="../../configs", version_base="1.3"):
+        cfg = compose(config_name="configs", overrides=args, return_hydra_config=True)
+        config_dict = OmegaConf.to_container(cfg, resolve=False)
+    return config_dict
+
+
+if __name__ == "__main__":
+    print(load_hydra_config())
