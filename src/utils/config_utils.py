91f176679debc07cafe591733bb10453ec4cd21f
wei <mai@mai0313.com>
2024-07-28 13:00:16 +0800

    chore: Update release_drafter.yml to include auto_click.exe in the files list

--- src/utils/config_utils.py
+++ src/utils/config_utils.py
@@ -6,7 +6,7 @@ from omegaconf import OmegaConf
 
 def load_hydra_config() -> dict:
     args = sys.argv[1:]
-    with initialize(config_path="../../configs", version_base="1.2"):
+    with initialize(config_path="../../configs", version_base="1.3"):
         cfg = compose(config_name="configs", overrides=args, return_hydra_config=False)
         config_dict = OmegaConf.to_container(cfg, resolve=False)
     return config_dict
