2d0a3dc3c08c34fc19e22124671fbf454ff4cfdd
wei <mai@mai0313.com>
2024-09-28 19:41:59 +0800

    refactor: refactor screenshot capture and logging handling
    ## What does this PR do?
    
    - Remove commented-out import statements for clarity
    - Replace `DeviceOutput` class with `Screenshot` across all relevant files
    - Replace `connect2adb` method with `get_screenshot`, updating its functionality to capture screenshots rather than connect to adb
    - Change log configuration to disable sending logs to 'logfire' and remove the log token
    - Update logging statements to include more contextual information such as window titles, URL, or serial numbers in screenshot methods
    - Refactor `ImageComparison` methods to make them private and streamline image processing
    - Streamline error handling and logging in methods dealing with remote and adb devices
    - Add informative docstrings to methods that have changed to maintain code documentation
    - Remove unused variables and commented-out code blocks to clean up the codebase
    - Correct typographical errors in variable names (`avaliable` to `available`) and improve readability by modifying method names and parameters
    
    Signed-off-by: wei <mai@mai0313.com>

--- src/utils/get_serial.py
+++ src/utils/get_serial.py
@@ -1,3 +1,4 @@
+import logfire
 from adbutils import adb
 from pydantic import Field, BaseModel, computed_field
 
@@ -13,23 +14,24 @@ class ADBDeviceManager(BaseModel):
 
     @computed_field
     @property
-    def avaliable_devices(self) -> list[int]:
+    def available_devices(self) -> list[int]:
         mumu_player = [16384, 16416]
         # ld_player = [5555, 5557]
-        avaliable_devices = [*mumu_player]
-        return avaliable_devices
+        available_devices = [*mumu_player]
+        return available_devices
 
     @computed_field
     @property
     def serials(self) -> list[str]:
-        for avaliable_device in self.avaliable_devices:
-            adb.connect(addr=f"{self.host}:{avaliable_device}", timeout=3.0)
+        for available_device in self.available_devices:
+            adb.connect(addr=f"{self.host}:{available_device}", timeout=3.0)
         devices = adb.device_list()
         serials = []
         for device in devices:
             if device.serial.startswith("emulator"):
                 continue
             serials.append(device.serial)
+        logfire.info("Available devices", serials=serials)
         return serials
 
     @computed_field
@@ -41,15 +43,16 @@ class ADBDeviceManager(BaseModel):
             running_app = device.app_current()
             running_details = AppInfo(**{"serial": serial, "package": running_app.package})
             running_apps.append(running_details)
+        logfire.info("Running apps", running_apps=running_apps)
         return running_apps
 
     def get_correct_serial(self) -> AppInfo:
-        apps = [
-            running_app for running_app in self.running_apps if running_app.package == self.target
-        ]
+        apps = [r_app for r_app in self.running_apps if r_app.package == self.target]
         if len(apps) > 1 or len(apps) == 0:
             raise Exception("Multiple or no devices found")
-        return apps[0]
+        app = next(iter(apps))
+        logfire.info("Correct device found", serial=app.serial, package=app.package)
+        return app
 
 
 if __name__ == "__main__":
