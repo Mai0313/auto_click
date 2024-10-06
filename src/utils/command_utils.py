1f9b7211e689a627015a8c5493950242b4238e8b
wei <mai@mai0313.com>
2024-10-06 14:20:43 +0800

    refactor: refactor logging and error handling in controllers
    ## What does this PR do?
    
    - Remove `logfire` dependency and related logging configurations across various files
    - Simplify error handling in `RemoteContoller` by removing specific logging
    - Alter the `RemoteContoller` class to possibly return `None` values in `find()` method when not confident
    - Remove specific log output statements related to debugging and error reporting in multiple methods across different files
    - Refactor methods by removing configuration decorators in `RemoteContoller` and `ImageComparison` classes
    
    Signed-off-by: wei <mai@mai0313.com>

--- src/utils/command_utils.py
+++ src/utils/command_utils.py
@@ -1,6 +1,5 @@
 import subprocess  # noqa: S404
 
-import logfire
 from pydantic import BaseModel
 
 
@@ -11,9 +10,6 @@ class CommandExecutor(BaseModel):
         connection_result = subprocess.run(self.commands, capture_output=True, text=True)  # noqa: S603
         stdout = connection_result.stdout
         stderr = connection_result.stderr
-        if stderr:
-            logfire.error("Error on executing", stderr=stderr, _exc_info=True)
-        logfire.info("Success on executing", stdout=stdout, _exc_info=True)
         return stdout, stderr
 
     def popen(self) -> tuple[str, str]:
@@ -21,7 +17,4 @@ class CommandExecutor(BaseModel):
             self.commands, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True
         )
         stdout, stderr = connection_result_popen.communicate()
-        if stderr:
-            logfire.error("Error on executing", stderr=stderr, _exc_info=True)
-        logfire.info("Success on executing", stdout=stdout, _exc_info=True)
         return stdout, stderr
