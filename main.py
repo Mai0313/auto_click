6add826cab514341da567f67d76caab264636374
wei <mai@mai0313.com>
2024-11-03 13:20:40 +0800

    feat: simplify codebase and enhance performance
    ## What does this PR do?
    
    - Remove the `mlflow` dependency and all related code across several files
    - Delete configuration files for general settings, and specific game settings in YAML format
    - Adjust automatic clicking logic to rely on found positions in `RemoteController` Python class
    - Reduce image comparison feature complexity by removing specific alignment calculations
    - Introduce new method `count_win_rate` to handle win rate counting based on found positions
    - Update saving mechanism to only support local storage in the `CustomLogger` class
    - Decrease confidence level for button click recognition in `all_stars.yaml` from `0.8` to `0.7`
    
    Signed-off-by: wei <mai@mai0313.com>

--- main.py
+++ main.py
@@ -4,20 +4,18 @@ import secrets
 import datetime
 
 import yaml
-import mlflow
 import logfire
 from adbutils import AdbDevice
-from pydantic import model_validator
+from pydantic import Field, model_validator
 import pyautogui
 from src.compare import ImageComparison
 from src.screenshot import GetScreen
 from src.types.game import GameResult
 from src.types.config import ConfigModel
-from src.utils.logger import CustomLogger
 from playwright.sync_api import Page
 from playwright.async_api import Page as APage
 from src.utils.get_serial import ADBDeviceManager
-from src.types.output_models import Screenshot, ShiftPosition
+from src.types.output_models import Screenshot, FoundPosition, ShiftPosition
 
 logfire.configure(send_to_logfire=False)
 
@@ -25,6 +23,8 @@ current_time = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
 
 
 class RemoteController(ConfigModel, GameResult):
+    found_result: FoundPosition = Field(default_factory=FoundPosition)
+
     @model_validator(mode="after")
     def _init_serial(self) -> "RemoteController":
         if self.serial:
@@ -37,12 +37,6 @@ class RemoteController(ConfigModel, GameResult):
         self.serial = apps.serial
         return self
 
-    @model_validator(mode="after")
-    def _init_mlflow(self) -> "RemoteController":
-        mlflow.set_tracking_uri("./mlruns")
-        mlflow.set_experiment(experiment_name=self.target)
-        return self
-
     async def get_screenshot(self) -> Screenshot:
         if self.target.startswith("http"):
             return await GetScreen.a_from_remote_window(url=self.target)
@@ -51,79 +45,59 @@ class RemoteController(ConfigModel, GameResult):
         # 返回 screenshot 和 shift_position，而不是 device
         return await GetScreen.from_exist_window(window_title=self.target)
 
-    async def click_button(
-        self,
-        device: Union[Page, APage, AdbDevice, ShiftPosition],
-        calibrated_x: int,
-        calibrated_y: int,
-        click_this: bool,
-    ) -> None:
-        if self.auto_click and click_this:
+    async def click_button(self, device: Union[Page, APage, AdbDevice, ShiftPosition]) -> None:
+        if self.found_result.calibrated_x and self.found_result.calibrated_y:
             if isinstance(device, Page):
-                device.mouse.click(x=calibrated_x, y=calibrated_y)
+                device.mouse.click(
+                    x=self.found_result.calibrated_x, y=self.found_result.calibrated_y
+                )
             elif isinstance(device, APage):
-                await device.mouse.click(x=calibrated_x, y=calibrated_y)
+                await device.mouse.click(
+                    x=self.found_result.calibrated_x, y=self.found_result.calibrated_y
+                )
             elif isinstance(device, AdbDevice):
-                device.click(x=calibrated_x, y=calibrated_y)
+                device.click(x=self.found_result.calibrated_x, y=self.found_result.calibrated_y)
             elif isinstance(device, ShiftPosition):
-                pyautogui.moveTo(x=calibrated_x, y=calibrated_y)
+                pyautogui.moveTo(
+                    x=self.found_result.calibrated_x, y=self.found_result.calibrated_y
+                )
                 pyautogui.click()
-            logfire.info(
-                "Button Found",
-                x=calibrated_x,
-                y=calibrated_y,
-                auto_click=self.auto_click,
-                click_this=click_this,
-            )
+
+    async def count_win_rate(self) -> None:
+        if self.found_result.found_button_name_en == "win":
+            self.win += 1
+            await self.a_export(today=current_time)
+        elif self.found_result.found_button_name_en == "lose":
+            self.lose += 1
+            await self.a_export(today=current_time)
 
     async def main(self) -> None:
-        with mlflow.start_run(run_name=current_time):
-            while True:
-                try:
-                    device_details = await self.get_screenshot()
-                    for config_dict in self.image_list:
-                        image_compare = ImageComparison(
-                            image_cfg=config_dict,
-                            screenshot=device_details.screenshot,
-                            device=device_details.device,
-                        )
-                        custom_logger = CustomLogger(original_image_path=config_dict.image_path)
-                        found = await image_compare.find_and_select(
-                            vertical_align="top", horizontal_align="right"
-                        )
-                        # found = await image_compare.find()
-                        if found.calibrated_x and found.calibrated_y:
-                            await self.click_button(
-                                device=device_details.device,
-                                calibrated_x=found.calibrated_x,
-                                calibrated_y=found.calibrated_y,
-                                click_this=config_dict.click_this,
-                            )
-
-                            if found.found_button_name_en == "win":
-                                self.win += 1
-                                await self.a_export(today=current_time)
-                            elif found.found_button_name_en == "lose":
-                                self.lose += 1
-                                await self.a_export(today=current_time)
-
-                            if config_dict.screenshot_option:
-                                await custom_logger.a_save_images(
-                                    images={
-                                        "color": found.color_screenshot,
-                                        "blackout": found.blackout_screenshot,
-                                    },
-                                    save_to="local",
-                                )
-                            await asyncio.sleep(config_dict.delay_after_click)
-                except Exception as e:
-                    _random_interval = secrets.randbelow(self.random_interval)
-                    logfire.error(
-                        f"Error: {e}, Retrying in {_random_interval} seconds", _exc_info=True
+        while True:
+            try:
+                device_details = await self.get_screenshot()
+                for config_dict in self.image_list:
+                    image_compare = ImageComparison(
+                        image_cfg=config_dict,
+                        screenshot=device_details.screenshot,
+                        device=device_details.device,
                     )
-                    await asyncio.sleep(_random_interval)
+
+                    self.found_result = await image_compare.find()
+
+                    if self.auto_click and config_dict.click_this:
+                        await self.click_button(device=device_details.device)
+
+                        await self.count_win_rate()
+
+                        await asyncio.sleep(config_dict.delay_after_click)
+            except Exception as e:
                 _random_interval = secrets.randbelow(self.random_interval)
+                logfire.error(
+                    f"Error: {e}, Retrying in {_random_interval} seconds", _exc_info=True
+                )
                 await asyncio.sleep(_random_interval)
+            _random_interval = secrets.randbelow(self.random_interval)
+            await asyncio.sleep(_random_interval)
 
 
 def load_yaml(config_path: str) -> dict:
