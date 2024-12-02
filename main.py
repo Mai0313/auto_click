faa7f903cb0b4d2cb7c861721fa75f759a4347e3
wei <mai@mai0313.com>
2024-12-03 01:20:43 +0800

    Refactor imports and update file paths in main.py

--- main.py
+++ main.py
@@ -1,33 +1,39 @@
 import json
-from typing import Any, Union
+from typing import Union
 import asyncio
+import logging
 from pathlib import Path
 import secrets
 import datetime
 
 import yaml
 import anyio
+import pandas as pd
 import logfire
 from adbutils import AdbDevice
-from pydantic import Field, computed_field, model_validator
+from pydantic import Field, model_validator
 import pyautogui
+from sqlalchemy import create_engine
 from playwright.sync_api import Page
 from playwright.async_api import Page as APage
 
 from src.compare import ImageComparison
 from src.screenshot import GetScreen
 from src.types.config import ConfigModel
+from src.types.database import DatabaseConfig
 from src.utils.get_serial import ADBDeviceManager
 from src.types.output_models import Screenshot, FoundPosition, ShiftPosition
 
 logfire.configure(send_to_logfire=False)
+logging.getLogger("sqlalchemy.engine.Engine").disabled = True
 
-current_time = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+start_time_string = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
 
 
 class RemoteController(ConfigModel):
     target_serial: str = Field(default="", description="The serial number of the target device.")
     found_result: FoundPosition = Field(default_factory=FoundPosition)
+    database: DatabaseConfig = Field(default_factory=DatabaseConfig)
 
     win: int = Field(default=0, title="Win", description="Number of wins")
     lose: int = Field(default=0, title="Lose", description="Number of loses")
@@ -38,24 +44,6 @@ class RemoteController(ConfigModel):
         apps = adb_manager.get_correct_serial()
         self.target_serial = apps.serial
         return self
-
-    @computed_field
-    @property
-    def win_lost_dict(self) -> dict[str, Any]:
-        return {
-            "win": self.win,
-            "lose": self.lose,
-            "total": self.win + self.lose,
-            "win_rate": self.win / (self.win + self.lose),
-            "lose_rate": self.lose / (self.win + self.lose),
-        }
-
-    async def export(self, today: str) -> dict[str, Any]:
-        log_path = Path("./logs")
-        log_path.mkdir(parents=True, exist_ok=True)
-        async with await anyio.open_file(f"./logs/{today}.json", "w") as f:
-            await f.write(json.dumps(self.win_lost_dict))
-        return self.win_lost_dict
 
     async def get_screenshot(self) -> Screenshot:
         if self.target.startswith("http"):
@@ -83,13 +71,33 @@ class RemoteController(ConfigModel):
                 )
                 pyautogui.click()
 
+    async def export_win_rate(self) -> None:
+        log_path = Path("./logs")
+        log_path.mkdir(parents=True, exist_ok=True)
+        total_games = self.win + self.lose
+        _current_time = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+        _current_date = datetime.datetime.now().strftime("%Y%m%d")
+        win_lost_dict = {
+            "date": _current_date,
+            "time": _current_time,
+            "win": self.win,
+            "lose": self.lose,
+            "total": total_games,
+            "win_rate": self.win / total_games,
+        }
+        async with await anyio.open_file(f"./logs/{start_time_string}.json", "w") as f:
+            await f.write(json.dumps(win_lost_dict))
+        engine = create_engine(self.database.postgres.postgres_dsn, echo=True)
+        data = pd.DataFrame([win_lost_dict])
+        data.to_sql(name="all_star_match_history", con=engine, if_exists="append", index=False)
+
     async def count_win_rate(self) -> None:
         if self.found_result.found_button_name_en == "win":
             self.win += 1
-            await self.export(today=current_time)
+            await self.export_win_rate()
         elif self.found_result.found_button_name_en == "lose":
             self.lose += 1
-            await self.export(today=current_time)
+            await self.export_win_rate()
 
     async def main(self) -> None:
         while True:
@@ -113,6 +121,7 @@ class RemoteController(ConfigModel):
                         await self.count_win_rate()
 
                         await asyncio.sleep(config_dict.delay_after_click)
+
             except Exception as e:
                 _random_interval = secrets.randbelow(self.random_interval)
                 logfire.error(
