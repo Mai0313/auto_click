00b646403be39f113bdfb98d6b4d8f3b776f9ad2
wei <mai@mai0313.com>
2024-12-04 01:40:39 +0800

    feat: implement image matching strategy configuration - Add strategy configuration for image matching in YAML files - Update image comparison logic to support the specified strategy - Refactor method signatures to include new parameters - Enhance image drawing methods to manage both color and blackout modes - Improve the path handling for images in the find method of image comparison - Update controller logic to pass the strategy to the find method
    Signed-off-by: wei <mai@mai0313.com>

--- src/compare.py
+++ src/compare.py
@@ -13,6 +13,10 @@ from .types.image_models import ImageModel
 from .types.output_models import FoundPosition, ShiftPosition
 
 
+class ImageComparisonMode(BaseModel):
+    pass
+
+
 class ImageComparison(BaseModel):
     """Represents an image comparison object.
 
@@ -35,20 +39,18 @@ class ImageComparison(BaseModel):
     screenshot: Union[Image.Image, bytes] = Field(..., description="The screenshot image")
     device: Union[Page, AdbDevice, ShiftPosition] = Field(..., description="The device")
 
-    async def save_images(self, images: dict[str, np.ndarray]) -> None:
+    async def save_images(self, image_type: str, screenshot: np.ndarray) -> None:
         log_dir = Path("./logs")
         log_dir.mkdir(exist_ok=True, parents=True)
-
-        for image_type, screenshot in images.items():
-            screenshot_path = (
-                log_dir / Path(self.image_cfg.image_path).with_suffix(f".{image_type}.png").name
-            )
-            if not screenshot_path.exists():
-                cv2.imwrite(str(screenshot_path.absolute()), screenshot)
+        screenshot_path = (
+            log_dir / Path(self.image_cfg.image_path).with_suffix(f".{image_type}.png").name
+        )
+        if not screenshot_path.exists():
+            cv2.imwrite(str(screenshot_path.absolute()), screenshot)
 
     async def __draw_rectangle(
         self,
-        color_screenshot: np.ndarray,
+        screenshot: np.ndarray,
         button_center_x: int,
         button_center_y: int,
         max_loc: cv2.typing.Point,
@@ -57,95 +59,78 @@ class ImageComparison(BaseModel):
         """Draws a rectangle on the image and saves the resulting image.
 
         Args:
-            color_screenshot (np.ndarray): The screenshot in color format.
+            screenshot (np.ndarray): The screenshot in color format.
             button_center_x (int): The x-coordinate of the button center.
             button_center_y (int): The y-coordinate of the button center.
             max_loc (cv2.typing.Point): The maximum location of the matched image.
             draw_black (bool): Flag indicating whether to draw a black rectangle.
 
         Returns:
-            color_screenshot (np.ndarray): The screenshot with the red rectangle drawn on it.
+            screenshot (np.ndarray): The screenshot with the red rectangle drawn on it.
         """
-        image_type = "color"
         matched_image_position = (button_center_x, button_center_y)
-        if draw_black:
-            image_type = "blackout"
-            # Create a mask with a white rectangle to keep the area inside the red box
-            masked_color_screenshot = np.zeros_like(color_screenshot)
-            cv2.rectangle(
-                masked_color_screenshot, max_loc, matched_image_position, (255, 255, 255), -1
-            )
-            # Create a fully black image
-            black_img = np.zeros_like(color_screenshot)
-            # Keep the red box area, black out the rest
-            color_screenshot = cv2.bitwise_and(
-                color_screenshot, masked_color_screenshot
-            ) + cv2.bitwise_and(black_img, cv2.bitwise_not(masked_color_screenshot))
 
         # Draw the red rectangle on the image
-        cv2.rectangle(color_screenshot, max_loc, matched_image_position, (0, 0, 255), 2)
-        await self.save_images(images={image_type: color_screenshot})
-        return color_screenshot
+        cv2.rectangle(screenshot, max_loc, matched_image_position, (0, 0, 255), 2)
+        await self.save_images(image_type="color", screenshot=screenshot)
 
-    async def __color_similarity_2d(self, image: np.ndarray, color: tuple) -> np.ndarray:
-        """Calculate the similarity between a given color and each pixel in a 2D image.
+        if draw_black:
+            # Create a mask with a white rectangle to keep the area inside the red box
+            masked_screenshot = np.zeros_like(screenshot)
+            cv2.rectangle(masked_screenshot, max_loc, matched_image_position, (255, 255, 255), -1)
+            # Create a fully black image
+            black_img = np.zeros_like(screenshot)
+            # Keep the red box area, black out the rest
+            screenshot = cv2.bitwise_and(screenshot, masked_screenshot) + cv2.bitwise_and(
+                black_img, cv2.bitwise_not(masked_screenshot)
+            )
+            await self.save_images(image_type="blackout", screenshot=screenshot)
 
-        Args:
-            image (np.ndarray): 2D array representing the image.
-            color (tuple): A tuple representing the RGB color (r, g, b).
+        # Crop the internal region of the rectangle
+        top_left = max_loc
+        bottom_right = (button_center_x, button_center_y)
+        cropped_region = screenshot[top_left[1] : bottom_right[1], top_left[0] : bottom_right[0]]
+        await self.save_images(image_type="cropped", screenshot=cropped_region)
 
-        Returns:
-            np.ndarray: A 2D array of uint8 values representing the similarity of each pixel to the given color.
-        """
-        # r, g, b = cv2.split(cv2.subtract(image, (*color, 0)))
-        # positive = cv2.max(cv2.max(r, g), b)
-        # r, g, b = cv2.split(cv2.subtract((*color, 0), image))
-        # negative = cv2.max(cv2.max(r, g), b)
-        # return cv2.subtract(255, cv2.add(positive, negative))
-        diff = cv2.subtract(image, (*color, 0))
-        r, g, b = cv2.split(diff)
-        cv2.max(r, g, dst=r)
-        cv2.max(r, b, dst=r)
-        positive = r
-        cv2.subtract((*color, 0), image, dst=diff)
-        r, g, b = cv2.split(diff)
-        cv2.max(r, g, dst=r)
-        cv2.max(r, b, dst=r)
-        negative = r
-        cv2.add(positive, negative, dst=positive)
-        cv2.subtract(255, positive, dst=positive)
-        return positive
+        return screenshot
 
-    async def find(self) -> FoundPosition:
+    async def find(self, strategy: str) -> FoundPosition:
         """Finds the position of a button image within a screenshot.
 
         Returns:
             FoundPosition: The found position of the button image.
         """
-        _image_path = Path(self.image_cfg.image_path)
-
         color_screenshot = np.array(self.screenshot)
         color_screenshot = cv2.cvtColor(color_screenshot, cv2.COLOR_RGB2BGR)
+        color_button_image = cv2.imread(self.image_cfg.image_path)
+
+        # Convert both images to grayscale
         gray_screenshot = cv2.cvtColor(color_screenshot, cv2.COLOR_BGR2GRAY)
+        button_image = cv2.cvtColor(color_button_image, cv2.COLOR_BGR2GRAY)
 
-        button_image = cv2.imread(_image_path.as_posix(), 0)
-
-        gray_matched = cv2.matchTemplate(gray_screenshot, button_image, cv2.TM_CCOEFF_NORMED)
+        # Match the button image with the screenshot
+        gray_matched = cv2.matchTemplate(
+            image=gray_screenshot,
+            templ=button_image,
+            method=getattr(cv2, strategy),
+            result=None,
+            mask=None,
+        )
 
         _, max_val, _, max_loc = cv2.minMaxLoc(gray_matched)
 
         if max_val > self.image_cfg.confidence:
             button_center_x = int(max_loc[0] + button_image.shape[1])
             button_center_y = int(max_loc[1] + button_image.shape[0])
-            cv2.imwrite("gray_screenshot.png", gray_screenshot)
-            cv2.imwrite("button_image.png", button_image)
+            # cv2.imwrite("gray_screenshot.png", gray_screenshot)
+            # cv2.imwrite("button_image.png", button_image)
 
             if self.image_cfg.screenshot_option:
                 tasks = []
                 for draw_black in [True, False]:
                     task = asyncio.create_task(
                         self.__draw_rectangle(
-                            color_screenshot=color_screenshot,
+                            screenshot=color_screenshot,
                             button_center_x=button_center_x,
                             button_center_y=button_center_y,
                             max_loc=max_loc,
@@ -158,7 +143,7 @@ class ImageComparison(BaseModel):
             return FoundPosition(
                 button_center_x=button_center_x,
                 button_center_y=button_center_y,
-                found_button_name_en=_image_path.stem,
+                found_button_name_en=Path(self.image_cfg.image_path).stem,
                 found_button_name_cn=self.image_cfg.image_name,
             )
         return FoundPosition()
--- src/controller.py
+++ src/controller.py
@@ -119,7 +119,7 @@ class RemoteController(ConfigModel):
                         device=device_details.device,
                     )
 
-                    self.found_result = await image_compare.find()
+                    self.found_result = await image_compare.find(strategy=self.strategy)
                     if self.auto_click and config_dict.click_this:
                         await self.click_button(device=device_details.device)
 
