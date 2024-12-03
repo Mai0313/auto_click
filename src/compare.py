d844b6c9cd39ebac679936a7db37f0a631d6533d
Wei <21039366+Mai0313@users.noreply.github.com>
2024-12-04 01:45:02 +0800

    Feat/add switch game (#63)
    * feat: refactor image handling and simplify game logic - Add new images for game selection conditions
    - Update dependencies to include a specific version of the OpenAI package
    - Remove logging related to image finding in the ImageComparison class
    - Streamline the found position return in the ImageComparison class
    - Simplify win/lose condition checks in the RemoteController class
    - Update method calls related to image comparison in the RemoteController class
    - Comment out unused configuration field in the ConfigModel class
    - Modify start.bat to correctly initiate the main script using PowerShell
    
    Signed-off-by: wei <mai@mai0313.com>
    
    * refactor: refactor screenshot handling and improve button selection - Remove unused asynchronous method for screenshot conversion.
    - Update method for finding and selecting button with new arguments for button center coordinates.
    - Simplify argument handling in the `click_button` method in the controller.
    - Adjust attributes in the `FoundPosition` model for improved clarity and simplicity.
    - Add a new method in `FoundPosition` to adjust button coordinates based on device shifts.
    - Update image handling process by ensuring the proper conversion of input screenshots.
    - Refactor the way screenshots are saved to enhance reliability.
    
    Signed-off-by: wei <mai@mai0313.com>
    
    * refactor: refactor screenshot handling and improve button selection - Remove unused asynchronous method for screenshot conversion.
    
    * refactor: refactor screenshot handling and improve button selection - Remove unused asynchronous method for screenshot conversion.
    
    * feat: implement image matching strategy configuration - Add strategy configuration for image matching in YAML files
    - Update image comparison logic to support the specified strategy
    - Refactor method signatures to include new parameters
    - Enhance image drawing methods to manage both color and blackout modes
    - Improve the path handling for images in the find method of image comparison
    - Update controller logic to pass the strategy to the find method
    
    Signed-off-by: wei <mai@mai0313.com>
    
    * fix: enhance error handling and logging in data processing - Add error handling when saving data to the database
    - Improve logging messages for exceptions
    - Refactor `data.to_sql` to use a multiline format for better readability
    
    Signed-off-by: wei <mai@mai0313.com>
    
    ---------
    
    Signed-off-by: wei <mai@mai0313.com>

--- src/compare.py
+++ src/compare.py
@@ -1,18 +1,20 @@
-from typing import Union, Literal
+from typing import Union
 import asyncio
 from pathlib import Path
 
 import cv2
 import numpy as np
-import logfire
 from pydantic import Field, BaseModel, ConfigDict
 import PIL.Image as Image
 from adbutils._device import AdbDevice
 from playwright.sync_api import Page
-from playwright.async_api import Page as APage
 
 from .types.image_models import ImageModel
 from .types.output_models import FoundPosition, ShiftPosition
+
+
+class ImageComparisonMode(BaseModel):
+    pass
 
 
 class ImageComparison(BaseModel):
@@ -37,308 +39,111 @@ class ImageComparison(BaseModel):
     screenshot: Union[Image.Image, bytes] = Field(..., description="The screenshot image")
     device: Union[Page, AdbDevice, ShiftPosition] = Field(..., description="The device")
 
-    async def __get_screenshot_array(self) -> tuple[np.ndarray, np.ndarray]:
-        """Converts the screenshot to color and grayscale arrays.
-
-        Returns:
-            color_screenshot (np.ndarray): The screenshot in color format.
-            gray_screenshot (np.ndarray): The screenshot in grayscale format.
-        """
-        color_screenshot = np.array(self.screenshot)
-        color_screenshot = cv2.cvtColor(color_screenshot, cv2.COLOR_RGB2BGR)
-        gray_screenshot = cv2.cvtColor(color_screenshot, cv2.COLOR_BGR2GRAY)
-        return color_screenshot, gray_screenshot
-
-    async def save_images(self, images: dict[Literal["color", "blackout"], np.ndarray]) -> None:
+    async def save_images(self, image_type: str, screenshot: np.ndarray) -> None:
         log_dir = Path("./logs")
         log_dir.mkdir(exist_ok=True, parents=True)
-
-        for image_type, screenshot in images.items():
-            screenshot_path = (
-                log_dir / Path(self.image_cfg.image_path).with_suffix(f".{image_type}.png").name
-            )
-            if not screenshot_path.exists():
-                cv2.imwrite(str(screenshot_path.absolute()), screenshot)
+        screenshot_path = (
+            log_dir / Path(self.image_cfg.image_path).with_suffix(f".{image_type}.png").name
+        )
+        if not screenshot_path.exists():
+            cv2.imwrite(str(screenshot_path.absolute()), screenshot)
 
     async def __draw_rectangle(
         self,
-        color_screenshot: np.ndarray,
-        matched_image_position: tuple[int, int],
+        screenshot: np.ndarray,
+        button_center_x: int,
+        button_center_y: int,
         max_loc: cv2.typing.Point,
         draw_black: bool,
     ) -> np.ndarray:
         """Draws a rectangle on the image and saves the resulting image.
 
         Args:
-            color_screenshot (np.ndarray): The screenshot in color format.
-            matched_image_position (tuple[int, int]): The position of the matched image.
+            screenshot (np.ndarray): The screenshot in color format.
+            button_center_x (int): The x-coordinate of the button center.
+            button_center_y (int): The y-coordinate of the button center.
             max_loc (cv2.typing.Point): The maximum location of the matched image.
             draw_black (bool): Flag indicating whether to draw a black rectangle.
 
         Returns:
-            color_screenshot (np.ndarray): The screenshot with the red rectangle drawn on it.
+            screenshot (np.ndarray): The screenshot with the red rectangle drawn on it.
         """
-        if draw_black:
-            # Create a mask with a white rectangle to keep the area inside the red box
-            masked_color_screenshot = np.zeros_like(color_screenshot)
-            cv2.rectangle(
-                masked_color_screenshot, max_loc, matched_image_position, (255, 255, 255), -1
-            )
-            # Create a fully black image
-            black_img = np.zeros_like(color_screenshot)
-            # Keep the red box area, black out the rest
-            color_screenshot = cv2.bitwise_and(
-                color_screenshot, masked_color_screenshot
-            ) + cv2.bitwise_and(black_img, cv2.bitwise_not(masked_color_screenshot))
+        matched_image_position = (button_center_x, button_center_y)
 
         # Draw the red rectangle on the image
-        cv2.rectangle(color_screenshot, max_loc, matched_image_position, (0, 0, 255), 2)
-        return color_screenshot
+        cv2.rectangle(screenshot, max_loc, matched_image_position, (0, 0, 255), 2)
+        await self.save_images(image_type="color", screenshot=screenshot)
 
-    async def find_and_select(
-        self,
-        vertical_align: Literal["top", "center", "bottom"] = "center",
-        horizontal_align: Literal["left", "center", "right"] = "center",
-    ) -> FoundPosition:
+        if draw_black:
+            # Create a mask with a white rectangle to keep the area inside the red box
+            masked_screenshot = np.zeros_like(screenshot)
+            cv2.rectangle(masked_screenshot, max_loc, matched_image_position, (255, 255, 255), -1)
+            # Create a fully black image
+            black_img = np.zeros_like(screenshot)
+            # Keep the red box area, black out the rest
+            screenshot = cv2.bitwise_and(screenshot, masked_screenshot) + cv2.bitwise_and(
+                black_img, cv2.bitwise_not(masked_screenshot)
+            )
+            await self.save_images(image_type="blackout", screenshot=screenshot)
+
+        # Crop the internal region of the rectangle
+        top_left = max_loc
+        bottom_right = (button_center_x, button_center_y)
+        cropped_region = screenshot[top_left[1] : bottom_right[1], top_left[0] : bottom_right[0]]
+        await self.save_images(image_type="cropped", screenshot=cropped_region)
+
+        return screenshot
+
+    async def find(self, strategy: str) -> FoundPosition:
         """Finds the position of a button image within a screenshot.
 
-        Args:
-            vertical_align (Literal['top', 'center', 'bottom'], optional): Vertical alignment within the matched rectangle. Defaults to 'center'.
-            horizontal_align (Literal['left', 'center', 'right'], optional): Horizontal alignment within the matched rectangle. Defaults to 'center'.
-
         Returns:
-            FoundPosition: An object containing the coordinates and other information.
-
-        Raises:
-            ValueError: If an invalid alignment is provided.
-
+            FoundPosition: The found position of the button image.
         """
-        color_screenshot, gray_screenshot = await self.__get_screenshot_array()
-        image_path = Path(self.image_cfg.image_path)
-        image_name_en = image_path.stem
-        image_name_cn = self.image_cfg.image_name
-        button_image = cv2.imread(image_path.as_posix(), 0)
+        color_screenshot = np.array(self.screenshot)
+        color_screenshot = cv2.cvtColor(color_screenshot, cv2.COLOR_RGB2BGR)
+        color_button_image = cv2.imread(self.image_cfg.image_path)
 
-        result = cv2.matchTemplate(gray_screenshot, button_image, cv2.TM_CCOEFF_NORMED)
-        _, max_val, _, max_loc = cv2.minMaxLoc(result)
+        # Convert both images to grayscale
+        gray_screenshot = cv2.cvtColor(color_screenshot, cv2.COLOR_BGR2GRAY)
+        button_image = cv2.cvtColor(color_button_image, cv2.COLOR_BGR2GRAY)
 
-        if max_val > self.image_cfg.confidence:
-            x = max_loc[0]
-            y = max_loc[1]
-            template_width = button_image.shape[1]
-            template_height = button_image.shape[0]
-
-            # Determine alignment positions within the matched rectangle
-            if horizontal_align == "left":
-                x_align = x
-            elif horizontal_align == "center":
-                x_align = x + template_width / 2
-            elif horizontal_align == "right":
-                x_align = x + template_width
-            else:
-                raise ValueError(f"Invalid horizontal_align: {horizontal_align}")
-
-            if vertical_align == "top":
-                y_align = y
-            elif vertical_align == "center":
-                y_align = y + template_height / 2
-            elif vertical_align == "bottom":
-                y_align = y + template_height
-            else:
-                raise ValueError(f"Invalid vertical_align: {vertical_align}")
-
-            # Define the small rectangle size
-            small_rect_size = 10  # Size of the small square
-            half_size = small_rect_size / 2
-
-            # Calculate the top-left and bottom-right points of the small rectangle
-            small_rect_top_left = (int(x_align - half_size), int(y_align - half_size))
-            small_rect_bottom_right = (int(x_align + half_size), int(y_align + half_size))
-
-            # Ensure the small rectangle is within image boundaries
-            img_height, img_width = color_screenshot.shape[:2]
-            small_rect_top_left = (max(0, small_rect_top_left[0]), max(0, small_rect_top_left[1]))
-            small_rect_bottom_right = (
-                min(img_width, small_rect_bottom_right[0]),
-                min(img_height, small_rect_bottom_right[1]),
-            )
-
-            # Draw the matched template rectangle (optional)
-            cv2.rectangle(
-                color_screenshot, (x, y), (x + template_width, y + template_height), (255, 0, 0), 2
-            )
-
-            # Draw the small rectangle at the specified alignment point
-            cv2.rectangle(
-                color_screenshot, small_rect_top_left, small_rect_bottom_right, (0, 0, 255), 2
-            )
-
-            # Calculate the center of the small rectangle
-            button_x = int((small_rect_top_left[0] + small_rect_bottom_right[0]) / 2)
-            button_y = int((small_rect_top_left[1] + small_rect_bottom_right[1]) / 2)
-
-            # Calibrate the coordinates if necessary
-            calibrated_x, calibrated_y = await self.calibrate(
-                button_center_x=button_x, button_center_y=button_y
-            )
-            # matched_image_position = (calibrated_x, calibrated_y)
-
-            # Update the blackout_screenshot if needed (depends on your implementation)
-            blackout_screenshot = color_screenshot.copy()
-            cv2.rectangle(
-                blackout_screenshot, small_rect_top_left, small_rect_bottom_right, (0, 0, 0), -1
-            )
-
-            # Logging and metrics (adjust as needed)
-            logfire.info(
-                f"Found {image_name_cn}",
-                button_x=button_x,
-                calibrated_x=calibrated_x,
-                button_y=button_y,
-                calibrated_y=calibrated_y,
-                button_name_en=image_name_en,
-                button_name_cn=image_name_cn,
-                button_file_path=image_path.as_posix(),
-                auto_click=self.image_cfg.click_this,
-            )
-            if self.image_cfg.screenshot_option:
-                await self.save_images(
-                    images={"color": color_screenshot, "blackout": blackout_screenshot}
-                )
-
-            return FoundPosition(
-                button_center_x=button_x,
-                calibrated_x=calibrated_x,
-                button_center_y=button_y,
-                calibrated_y=calibrated_y,
-                found_button_name_en=image_name_en,
-                found_button_name_cn=image_name_cn,
-                color_screenshot=color_screenshot,
-                blackout_screenshot=blackout_screenshot,
-            )
-        return FoundPosition(
-            button_center_x=None,
-            calibrated_x=None,
-            button_center_y=None,
-            calibrated_y=None,
-            found_button_name_en=None,
-            found_button_name_cn=None,
-            color_screenshot=None,
-            blackout_screenshot=None,
+        # Match the button image with the screenshot
+        gray_matched = cv2.matchTemplate(
+            image=gray_screenshot,
+            templ=button_image,
+            method=getattr(cv2, strategy),
+            result=None,
+            mask=None,
         )
 
-    async def find(self) -> FoundPosition:
-        """Finds the position of a button image within a screenshot.
-
-        Returns:
-            button_center_x (int): The x-coordinate of the button center.
-            button_center_y (int): The y-coordinate of the button center.
-            color_screenshot (np.ndarray): The screenshot with the red rectangle drawn on it.
-
-        Todo:
-            Add logging functionality
-        """
-        button_center_x, button_center_y = 0, 0
-        color_screenshot, gray_screenshot = await self.__get_screenshot_array()
-        image_path = Path(self.image_cfg.image_path)
-        image_name_en = image_path.stem
-        image_name_cn = self.image_cfg.image_name
-        button_image = cv2.imread(image_path.as_posix(), 0)
-
-        result = cv2.matchTemplate(gray_screenshot, button_image, cv2.TM_CCOEFF_NORMED)
-        _, max_val, _, max_loc = cv2.minMaxLoc(result)
+        _, max_val, _, max_loc = cv2.minMaxLoc(gray_matched)
 
         if max_val > self.image_cfg.confidence:
             button_center_x = int(max_loc[0] + button_image.shape[1])
             button_center_y = int(max_loc[1] + button_image.shape[0])
+            # cv2.imwrite("gray_screenshot.png", gray_screenshot)
+            # cv2.imwrite("button_image.png", button_image)
 
-            calibrated_x, calibrated_y = await self.calibrate(
-                button_center_x=button_center_x, button_center_y=button_center_y
-            )
-            matched_image_position = (calibrated_x, calibrated_y)
-
-            tasks = [
-                asyncio.create_task(
-                    self.__draw_rectangle(
-                        color_screenshot=color_screenshot,
-                        matched_image_position=matched_image_position,
-                        max_loc=max_loc,
-                        draw_black=False,
-                    )
-                ),
-                asyncio.create_task(
-                    self.__draw_rectangle(
-                        color_screenshot=color_screenshot,
-                        matched_image_position=matched_image_position,
-                        max_loc=max_loc,
-                        draw_black=True,
-                    )
-                ),
-            ]
-
-            blackout_screenshot, color_screenshot = await asyncio.gather(*tasks)
-
-            logfire.info(
-                f"Found {image_name_cn}",
-                button_center_x=button_center_x,
-                calibrated_x=calibrated_x,
-                button_center_y=button_center_y,
-                calibrated_y=calibrated_y,
-                button_name_en=image_name_en,
-                button_name_cn=image_name_cn,
-                button_file_path=image_path.as_posix(),
-                auto_click=self.image_cfg.click_this,
-            )
             if self.image_cfg.screenshot_option:
-                await self.save_images(
-                    images={"color": color_screenshot, "blackout": blackout_screenshot}
-                )
+                tasks = []
+                for draw_black in [True, False]:
+                    task = asyncio.create_task(
+                        self.__draw_rectangle(
+                            screenshot=color_screenshot,
+                            button_center_x=button_center_x,
+                            button_center_y=button_center_y,
+                            max_loc=max_loc,
+                            draw_black=draw_black,
+                        )
+                    )
+                    tasks.append(task)
+
+                await asyncio.gather(*tasks)
             return FoundPosition(
                 button_center_x=button_center_x,
-                calibrated_x=calibrated_x,
                 button_center_y=button_center_y,
-                calibrated_y=calibrated_y,
-                found_button_name_en=image_name_en,
-                found_button_name_cn=image_name_cn,
-                color_screenshot=color_screenshot,
-                blackout_screenshot=blackout_screenshot,
+                found_button_name_en=Path(self.image_cfg.image_path).stem,
+                found_button_name_cn=self.image_cfg.image_name,
             )
-        return FoundPosition(
-            button_center_x=None,
-            calibrated_x=None,
-            button_center_y=None,
-            calibrated_y=None,
-            found_button_name_en=None,
-            found_button_name_cn=None,
-            color_screenshot=None,
-            blackout_screenshot=None,
-        )
-
-    async def calibrate(self, button_center_x: int, button_center_y: int) -> tuple[int, int]:
-        """Adjusts the button center coordinates based on the device type.
-
-        Args:
-            button_center_x (int): The x-coordinate of the button center.
-            button_center_y (int): The y-coordinate of the button center.
-
-        Returns:
-            tuple[int, int]: The adjusted button center coordinates.
-        """
-        # if the device is from a window process, we need to add shift_x, shift_y to the button_center_x, button_center_y
-        # since we do not know the exact position of the window.
-        if isinstance(self.device, Page):
-            # button_center_x = button_center_x // 2
-            # button_center_y = button_center_y // 2
-            pass
-        if isinstance(self.device, APage):
-            # button_center_x = button_center_x // 2
-            # button_center_y = button_center_y // 2
-            pass
-        if isinstance(self.device, AdbDevice):
-            # button_center_x = button_center_x // 2
-            # button_center_y = button_center_y // 2
-            pass
-        if isinstance(self.device, ShiftPosition):
-            button_center_x = button_center_x + self.device.shift_x
-            button_center_y = button_center_y + self.device.shift_y
-        return button_center_x, button_center_y
+        return FoundPosition()
--- src/types/output_models.py
+++ src/types/output_models.py
@@ -2,7 +2,6 @@ from typing import Union, Optional
 from pathlib import Path
 
 from PIL import Image
-import numpy as np
 from pydantic import Field, BaseModel, ConfigDict
 from adbutils._device import AdbDevice
 from playwright.sync_api import Page
@@ -27,8 +26,10 @@ class FoundPosition(BaseModel):
     Attributes:
         button_center_x (Optional[int]): The x-coordinate of the button center.
         button_center_y (Optional[int]): The y-coordinate of the button center.
-        color_screenshot (cv2.typing.MatLike): The screenshot of the button in color.
-        blackout_screenshot (cv2.typing.MatLike): The screenshot of the button with blackout effect.
+        found_button_name_en (Optional[str]): The name of the found button in English.
+        found_button_name_cn (Optional[str]): The name of the found button in Chinese.
+        color_screenshot (Optional[np.ndarray]): The screenshot of the button in color.
+        blackout_screenshot (Optional[np.ndarray]): The screenshot of the button with a blackout effect.
     """
 
     model_config = ConfigDict(arbitrary_types_allowed=True)
@@ -36,25 +37,13 @@ class FoundPosition(BaseModel):
     button_center_x: Optional[int] = Field(
         default=None,
         description="The x-coordinate of the button center.",
-        frozen=True,
-        deprecated=False,
-    )
-    calibrated_x: Optional[int] = Field(
-        default=None,
-        description="The calibrated x-coordinate of the button center.",
-        frozen=True,
+        frozen=False,
         deprecated=False,
     )
     button_center_y: Optional[int] = Field(
         default=None,
         description="The y-coordinate of the button center.",
-        frozen=True,
-        deprecated=False,
-    )
-    calibrated_y: Optional[int] = Field(
-        default=None,
-        description="The calibrated y-coordinate of the button center.",
-        frozen=True,
+        frozen=False,
         deprecated=False,
     )
     found_button_name_en: Optional[str] = Field(
@@ -69,18 +58,17 @@ class FoundPosition(BaseModel):
         frozen=True,
         deprecated=False,
     )
-    color_screenshot: Optional[np.ndarray] = Field(
-        default=None,
-        description="The screenshot of the button in color.",
-        frozen=True,
-        deprecated=False,
-    )
-    blackout_screenshot: Optional[np.ndarray] = Field(
-        default=None,
-        description="The screenshot of the button with a blackout effect.",
-        frozen=True,
-        deprecated=False,
-    )
+
+    async def calibrate(self, shift_x: int, shift_y: int) -> None:
+        """Adjusts the button center coordinates based on the device type.
+
+        Args:
+            shift_x (int): The amount to shift the button center along the x-axis.
+            shift_y (int): The amount to shift the button center along the y-axis.
+        """
+        if self.button_center_x and self.button_center_y:
+            self.button_center_x += shift_x
+            self.button_center_y += shift_y
 
 
 class Screenshot(BaseModel):
@@ -106,19 +94,19 @@ class Screenshot(BaseModel):
         Args:
             save_path (str): The path where the screenshot will be saved. If the path does not end with ".png", it will be automatically appended.
 
+        Raises:
+            TypeError: If the screenshot type is not supported.
+
         Returns:
             str: The full path where the screenshot was saved.
         """
         output_path = (
             Path(f"{save_path}.png") if not save_path.endswith(".png") else Path(save_path)
         )
-        await self.__save_image(output_path=output_path)
-        return output_path.absolute().as_posix()
-
-    async def __save_image(self, output_path: Path) -> None:
         if isinstance(self.screenshot, bytes):
             output_path.write_bytes(self.screenshot)
         elif isinstance(self.screenshot, Image.Image):
             self.screenshot.save(output_path)
         else:
             raise TypeError("不支持的屏幕截图类型。")
+        return output_path.absolute().as_posix()
